4f559368be24a74eb6a10dfeef604d7902d4bd54
Michael Nichols <mumptastic@gmail.com>
2021-03-03 13:07:01 -0600

    added styles to layout --wip

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import { Link}  from 'gatsby'
 import { title } from './layout.module.css'
+import { navBar } from './layout.module.css'
 
 const Layout = ({ children, pageTitle }) => {
     return (
         <div>
             <title>{pageTitle}</title>
             <h1 className={title}>Hi, I'm Michael</h1>
-            <Link to='/'>Home</Link>
-            <Link to='/about'>About</Link>
+            <nav className={navBar}>
+                <ul>
+                    <li><Link to='/'>Home</Link></li>
+                    <li><Link to='/about'>About</Link></li>
+                </ul>
+            </nav>
+            
             { children }
         </div>
     )
