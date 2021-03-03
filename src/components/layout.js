a403a9b3e30891a7398247ce0b62bb8d1c464246
Michael Nichols <mumptastic@gmail.com>
2021-03-03 14:37:02 -0600

    added a blog -- wip

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,20 +1,33 @@
 import React from 'react'
-import { Link}  from 'gatsby'
-import { title } from './layout.module.css'
-import { navBar } from './layout.module.css'
+import { Link, useStaticQuery, graphql}  from 'gatsby'
+import { title, navBar } from './layout.module.css'
+
 
 const Layout = ({ children, pageTitle }) => {
+
+    const data = useStaticQuery(graphql`
+        query {
+            site {
+                siteMetadata {
+                  title
+                  description
+                }
+            }
+        }
+    `)
+
     return (
         <div>
-            <title>{pageTitle}</title>
+            <title>{pageTitle} | {data.site.siteMetadata.title}</title>
             <h1 className={title}>Hi, I'm Michael</h1>
             <nav className={navBar}>
                 <ul>
                     <li><Link to='/'>Home</Link></li>
                     <li><Link to='/about'>About</Link></li>
+                    <li><Link to='/blog'>Blog</Link></li>
                 </ul>
             </nav>
-            
+            <blockquote>{data.site.siteMetadata.description}</blockquote>
             { children }
         </div>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import { StaticImage } from "gatsby-plugin-image"
 
+
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home">
