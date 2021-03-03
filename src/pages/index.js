b548d8c1414c9b5c84d2cdf0d4904a7ed7363c48
Michael Nichols <mumptastic@gmail.com>
2021-03-03 13:30:44 -0600

    added image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Layout from "../components/layout"
+import { StaticImage } from "gatsby-plugin-image"
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home">
      <h2>Home Page</h2>
+     <StaticImage src="https://pbs.twimg.com/media/Evkn6fRWgAgxOWe?format=jpg&amp;name=4096x4096" alt="Laurie's doggo sitting in the sun." />
     </Layout>
   )
 }
